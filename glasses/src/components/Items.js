7e5f573749567bf3f2426e640a2d8af53c8930e6
Thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-03-02 12:09:05 +0700

    refactor code

--- glasses/src/components/Items.js
+++ glasses/src/components/Items.js
@@ -1,83 +1,18 @@
 import React, { Component } from "react";
 import "./item.css";
+import glasses from './data/glasses.json'
 
 class Items extends Component {
+  listGlasses = glasses;
   state = {
     isTry: false,
-    url: "",
-    name: "",
-    desc: "",
+    glassTry : {}
   };
-  listGlasses = [
-    {
-      id: 1,
-      price: 30,
-      name: "GUCCI G8850U",
-      url: "./glassesImage/v1.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 2,
-      price: 50,
-      name: "GUCCI G8759H",
-      url: "./glassesImage/v2.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 3,
-      price: 30,
-      name: "DIOR D6700HQ",
-      url: "./glassesImage/v3.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 4,
-      price: 70,
-      name: "DIOR D6005U",
-      url: "./glassesImage/v4.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 5,
-      price: 40,
-      name: "PRADA P8750",
-      url: "./glassesImage/v5.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 6,
-      price: 60,
-      name: "PRADA P9700",
-      url: "./glassesImage/v6.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 7,
-      price: 80,
-      name: "FENDI F8750",
-      url: "./glassesImage/v7.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 8,
-      price: 100,
-      name: "FENDI F8500",
-      url: "./glassesImage/v8.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-    {
-      id: 9,
-      price: 60,
-      name: "FENDI F4300",
-      url: "./glassesImage/v9.png",
-      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-    },
-  ];
   renderListGlasses = () => {
-    let lstItemUIs = this.listGlasses.map((item) => {
+    let lstItemUIs = this.listGlasses.map((item,index) => {
       return (
-        <button key={item.id} className="glass btn col-2" onClick={() => {
-          this.handerTryGlass(item.id);
+        <button key={index} className="glass btn col-2" onClick={() => {
+          this.handerTryGlass(item);
         }}>
           <img
             className="img-btn-glass"
@@ -92,29 +27,26 @@ class Items extends Component {
 
   showGlassOnModel = () => {
     if(this.state.isTry){
+      const {url,name,desc} = this.state.glassTry
       return (
         <div className="card">
-          <img src={this.state.url} alt="" className="card-img"></img>
+          <img src={url} alt="" className="card-img"></img>
           <div className="card-body">
-            <h5>{this.state.name}</h5>
-            <p>{this.state.desc}</p>
+            <h5>{name}</h5>
+            <p>{desc}</p>
           </div>
         </div>
       );
     }
   };
 
-  handerTryGlass = (id) => {
-    let item = this.listGlasses.find((item) => item.id === id);
-    if (item !== -1) {
+  handerTryGlass = (item) => {
       this.setState({
         isTry: true,
-        url: item.url,
-        name: item.name,
-        desc: item.desc,
+        glassTry : item
       })
     }
-  };
+
 
   render() {
     return (
@@ -124,5 +56,6 @@ class Items extends Component {
       </div>
     );
   }
-}
+} 
+
 export default Items;
